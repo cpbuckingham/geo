0d7a951979b85bfa2a8b9855071016ec501e71c4
Cam <cameron.p.buckingham@gmail.com>
2016-06-27 16:27:19 -0500

    jquery and smooth scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,144 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+
+	"use strict";
+
+    /************** Nav Scripts **************/
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 1) {
+            $('nav').addClass('sticky-nav');
+        } else {
+            $('nav').removeClass('sticky-nav');
+        }
+    });
+
+    $('a').click(function() {
+        if ($(this).attr('href') === '#') {
+            return false;
+        }
+    });
+
+    // Margin on the menu to make room for sidebar menu if it exists
+
+    if ($('.sidebar-menu-toggle').length && !$('.sidebar-menu-toggle i').hasClass('variant-deleted-mrv')) {
+        $('nav').find('.menu').css('margin-right', 32);
+    }
+
+    // Mobile menu toggle
+
+    $('.mobile-menu-toggle').click(function() {
+        $('nav').toggleClass('open-menu');
+    });
+
+    // Sidebar menu toggle
+
+    $('.sidebar-menu-toggle').click(function() {
+        $('.sidebar-menu').toggleClass('show-sidebar');
+        $('.main-container').toggleClass('reveal-sidebar');
+        $('nav .container').toggleClass('reveal-sidebar');
+    });
+
+    $('.main-container').click(function() {
+        if ($('.sidebar-menu').hasClass('show-sidebar')) {
+            $('.sidebar-menu').toggleClass('show-sidebar');
+            $('.main-container').toggleClass('reveal-sidebar');
+            $('nav .container').toggleClass('reveal-sidebar');
+        }
+    });
+
+
+    /************** Divider Scripts **************/
+
+    $('.background-image-holder').each(function() {
+
+        // Append background-image <img>'s as li item CSS background for better responsive performance
+        var imgSrc = $(this).children('.background-image').attr('src');
+        $(this).css('background', 'url("' + imgSrc + '")');
+        $(this).children('.background-image').hide();
+        $(this).css('background-position', '50% 0%');
+        // Check if the slider has a color scheme attached, if so, apply it to the slider nav
+    });
+
+
+
+    /************** Twitter Feed **************/
+
+    $('.tweets-feed').each(function(index) {
+        $(this).attr('id', 'tweets-' + index);
+    }).each(function(index) {
+
+        function handleTweets(tweets) {
+            var x = tweets.length;
+            var n = 0;
+            var element = document.getElementById('tweets-' + index);
+            var html = '<ul class="slides">';
+            while (n < x) {
+                html += '<li>' + tweets[n] + '</li>';
+                n++;
+            }
+            html += '</ul>';
+            element.innerHTML = html;
+            return html;
+        }
+
+        twitterFetcher.fetch($('#tweets-' + index).attr('data-widget-id'), '', 5, true, true, true, '', false, handleTweets);
+
+    });
+
+    /************** Countdown Timer **************/
+
+    $('.countdown').each(function() {
+        $(this).countdown({
+            until: new Date($(this).attr('data-date'))
+        });
+    });
+
+});
+
+$(window).load(function() {
+
+	"use strict";
+
+    var navHeight = $('nav').outerHeight();
+    /************** Parallax Scripts **************/
+
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    var isIE = /*@cc_on!@*/ false || !!document.documentMode;
+    var isChrome = !!window.chrome;
+    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    var prefix;
+
+    if (isFirefox) {
+        prefix = '-moz-';
+    } else if (isIE) {
+
+    } else if (isChrome || isSafari) {
+        prefix = '-webkit-';
+    }
+
+    $('.parallax-background').each(function() {
+        $(this).attr('data-bottom-top', prefix + 'transform: translate3d(0px,-100px, 0px)');
+        $(this).attr('data-center', prefix + 'transform: translate3d(0px,0px, 0px)');
+        $(this).attr('data-top-bottom', prefix + 'transform: translate3d(0px,100px, 0px)');
+    });
+
+    if (!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+        skrollr.init({
+            forceHeight: false
+        });
+    }
+
+
+
+    // setTimeout(function() {
+    //     setTimeout(function() {
+    //         $('.main-container').addClass('show-content');
+    //         $('nav').addClass('show-content');
+    //     }, 500);
+    // }, 10);
+
+
+});
