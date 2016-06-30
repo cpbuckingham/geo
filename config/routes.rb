94bbc2fc54603f7dc15ea944434486398c3bbaf8
Cam <cameron.p.buckingham@gmail.com>
2016-06-30 17:57:50 -0500

    issue with modal outlined

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root "application#application"
-resources :users
+   resources :users
 
 
   # Example of regular route:
