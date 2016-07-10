43838f1a25f0642b5865e736c7a10c08b4458e8e
Cam <cameron.p.buckingham@gmail.com>
2016-07-10 15:56:27 -0500

    log in and log out working

--- db/migrate/20160710161223_create_users.rb
+++ db/migrate/20160710161223_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
       t.string :email
-      t.string :encrypted_password
-      t.string :salt
-      t.timestamps
+      t.string :password_hash
+      t.string :password_salt
+
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629182256) do
+ActiveRecord::Schema.define(version: 20160710161223) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",           limit: 255
-    t.string   "email",              limit: 255
-    t.string   "encrypted_password", limit: 255
-    t.string   "salt",               limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "email",         limit: 255
+    t.string   "password_hash", limit: 255
+    t.string   "password_salt", limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
